0eb63eac896a19e3f2875ebf2f984430a583bbd1
qwdltjdals <iu_love@kakao.com>
2024-06-18 17:25:20 +0900

    ddd

---
+++ javascript/todolist.js
@@ -0,0 +1,137 @@
+let todoList = [];
+loadTodoList();
+
+function handleTodoInputKeyDown(e) {
+    if (e.keyCode === 13) {
+        handleTodoOkClick();
+    }
+}
+
+function handleTodoOkClick(e) {
+    const todoInput = document.querySelector(".todo-input");
+    if (isBlank(todoInput)) {
+        alert("내용을 입력하세요.");
+        clearTodoInput();
+        return;
+    }
+    addTodo();
+    clearTodoInput();
+}
+
+function addTodo() {
+    const todo = {
+        id: createNewId(),
+        content: document.querySelector(".todo-input").value,
+        date: transformDate(new Date())
+    }
+
+    todoList = [...todoList, todo];
+    saveLocalStorage();
+    loadTodoList();
+}
+
+function createNewId() {
+    const todoIdList = todoList.map(todo => todo.id);
+    const maxId = !todoList.length ? 0 : Math.max.apply(null, todoIdList);
+    return maxId + 1;
+}
+
+function saveLocalStorage() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function loadTodoList() {
+    const lsTodoList = localStorage.getItem("todoList");
+    todoList = !lsTodoList ? [] : JSON.parse(lsTodoList);
+    renderTodoList();
+}
+
+function renderTodoList() {
+    const todoListContainer = document.querySelector(".todo-list-container");
+    todoListContainer.innerHTML = todoList.map(todo => {
+        return `
+         <li class="todo-card">
+                    <h3 class="todo-date">${todo.date}</h3>
+                    <p class="todo-content">${todo.content}</p>
+                    <div class="todo-buttons">
+                        <button class="button edit-button" onclick="handleEditClick(event)" value="${todo.id}">수정</button>
+                        <button class="button delite-button" onclick="handleDeleteClick(event)" value="${todo.id}">삭제</button>
+                    </div>
+                </li>
+                `;
+    }).join("");
+}
+
+function clearTodoInput() {
+    const todoInput = document.querySelector(".todo-input");
+    todoInput.value = "";
+    todoInput.focus();
+}
+
+function isBlank(input) {
+    return !input.value.replaceAll(" ", "");
+}
+
+function transformDate(date) {
+    const dayList = ["일", "월", "화", "수", "목", "금", "토"];
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.(${dayList[date.getDay()]}) ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+function handleDeleteClick(e) {
+    if (confirm("정말로 삭제하시겠습니까?")) {
+        todoList = todoList.filter(todo => todo.id !== parseInt(e.target.value));
+        saveLocalStorage();
+        loadTodoList();
+    }
+
+}
+
+function handleEditClick(e) {
+    const element = `
+ <div class="modal" onclick="handleModalBackgroundClick()">
+        <div class="modal-eidt-container" onclick="event.stopPropagation()">
+            <h3 class="modal-title">TODO 수정하기</h3>
+            <div class="input-box">
+                <input type="text" class="todo-input" onKeyDown="if(enent.keyCode === 13) document.querySelector('.modal button:nth-of-type(1)').click()">
+            </div>
+            <div class="todo-buttons">
+                <button class="button" onclick="handleEditOkClick(event)" value="${e.target.value}">확인</button>
+                <button class="button" onclick="closeModal()">취소</button>
+            </div>
+        </div>
+`;
+    openModal(element);
+}
+
+function handleEditOkClick(e) {
+    todoList = todoList.map(todo => {
+        if(todo.id === parseInt(e.target.value)) {
+            return {
+                ...todo,
+                content: document.querySelector(".modal .todo-input").value,
+                date: transformDate(new Date)
+            }
+        }
+        return todo;
+    })
+    saveLocalStorage();
+    CloseModal();
+    loadTodoList();
+}
+
+function handleModalBackgroundClick() {
+    CloseModal();
+}
+
+function openModal(element) {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("modal-show");
+    modal.innerHTML = element;
+}
+
+function CloseModal() {
+    const modal = document.querySelector(".modal");
+    modal.innerHTML = "";
+    modal.classList.remove("modal-show");
+
+}
--- javascript/user_management.js
+++ javascript/user_management.js
@@ -22,11 +22,11 @@ function randerTable() {
     userTableBody.innerHTML = userList.map(({ id, name, username, password }, index) => {
         return `
             <tr>
-            <th><input type="checkbox" onchange="handleUserCheck(event)"></th>
+            <th><input type="checkbox" onchange="handleUserCheck(event)" value="${id}"></th>
             <td>${index + 1}</td>
             <td>${id}</td>
-            <td>${name}</td>
             <td>${username}</td>
+            <td>${name}</td>
             <td>${password}</td>
             <th><button onclick="deleteUser(event)" value="${id}">삭제</button></th>
             </tr>
@@ -57,13 +57,27 @@ function handleUserInputKeyDown(e) {
             passwordInput.focus();
         }
         if (e.target.name === "password") {
-            userList = [...userList, { ...user, id: getNewId() } ]; // 기존의 유저리스트 정보의 맨 뒤에 새로운 유저를 추가한다.
+            if (inputMode === 1) {
+                userList = [...userList, { ...user, id: getNewId() }]; // 기존의 유저리스트 정보의 맨 뒤에 새로운 유저를 추가한다.
+            }
+            if (inputMode === 2) {
+                let findIndex = -1;
+                for (let i = 0; i < userList.length; i++) {
+                    if (userList[i].id === user.id) {
+                        findIndex = i;
+                        break;
+                    }
+                }
+                if (findIndex === -1) {
+                    alert("사용자 정보 수정 중 오류 발생. 관리자에게 문의하세요.")
+                    return;
+                }
+                userList[findIndex] = user;
+            }
+
             saveUserList()
             randerTable();
-
-            usernameInput.value = emptyUser.username;
-            nameInput.value = emptyUser.name;
-            passwordInput.value = emptyUser.password;
+            clearInputValue();
 
             usernameInput.focus();
         }
@@ -76,14 +90,14 @@ function saveUserList() {
 
 function loadUserList() {
     const lsuserList = localStorage.getItem("userList");
-    userList = !lsuserList ? [] : JSON.parse(lsuserList);   
+    userList = !lsuserList ? [] : JSON.parse(lsuserList);
     randerTable()
 
 }
 
 function deleteUser(e) {
-    userList = userList.filter(({id}) => id !== parseInt(e.target.value)); // !==는 문자열 비교는 불가능 = int로 바꿈 parseInt
-    saveUserList();
+    userList = userList.filter(({ id }) => id !== parseInt(e.target.value)); // !==는 문자열 비교는 불가능 = int로 바꿈 parseInt 
+    saveUserList(); // 서로 일치하지 않으면 새로 담아라 = 삭제할 애만 빼고 다시 배열을 담아라
     randerTable();
 }
 
@@ -101,4 +115,49 @@ function handleUserCheck(e) {
 
         일단 전체를 다 체크 해제를 시킨 후, 조건문 if 를 통해서 현재 타겟을 체크
     */
+
+    const checkBoxList = document.querySelectorAll("input[type='checkbox']");
+    for (let checkBox of checkBoxList) { // 자바스크립트에서의 포이치문
+        if (checkBox === e.target) {
+            continue;
+        }
+        checkBox.checked = false;
+    }
+
+    if (e.target.checked) {
+        inputMode = 2;
+        const [findUser] = userList.filter(user => user.id === parseInt(e.target.value));// value는 스트링이라서 int로 바꿔서 전달
+        setInputValue(findUser);
+        user = {
+            ...findUser
+        }
+        return;
+    }
+    clearInputValue();
+
+}
+
+function setInputValue(user) {
+    const nameInput = document.querySelector(".name-input");
+    const usernameInput = document.querySelector(".username-input");
+    const passwordInput = document.querySelector(".password-input");
+
+
+    nameInput.value = user.name;
+    usernameInput.value = user.username;
+    passwordInput.value = user.password;
+}
+
+function clearInputValue() {
+    const nameInput = document.querySelector(".name-input");
+    const usernameInput = document.querySelector(".username-input");
+    const passwordInput = document.querySelector(".password-input");
+    usernameInput.value = emptyUser.username;
+    nameInput.value = emptyUser.name;
+    passwordInput.value = emptyUser.password;
+    
+    inputMode = 1;
+    user = {
+        ...emptyUser
+    }
 }
